5dc9bd1b698ecc018a07e9638628d85cdb656d23
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2023-10-25 10:35:32 +0530

    update light and functionality work with management routes only other routes will work on light mode

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import ApplicationRoutes from "routes";
 
 // ** React Perfect Scrollbar Style
 import 'react-perfect-scrollbar/dist/css/styles.css'
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+
+  const location = useLocation();
+
+  // Check if the current pathname matches the pattern
+  const isManagementRoute =  location.pathname.startsWith('/management');
   
   return (
     <SettingsProvider>
         <SettingsConsumer>
           {({ settings }) => {
+            if(!isManagementRoute){
+              settings = {...settings,mode:'light'}
+            }
             return <ThemeComponent settings={settings}>
                 <ApplicationRoutes/>
             </ThemeComponent>
