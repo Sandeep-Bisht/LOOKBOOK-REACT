04100c10831009f4041a3acc3386465bbafefe0a
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2023-12-06 18:17:38 +0530

    update dark and light mode theme functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { SettingsConsumer, SettingsProvider } from '@core/context/settingsContext'
-import ThemeComponent from '@core/theme/ThemeComponent'
 import ApplicationRoutes from "routes";
 import 'css/common.css'
 
@@ -8,29 +6,5 @@ import 'css/common.css'
 import 'react-perfect-scrollbar/dist/css/styles.css'
 import { RouterProvider } from "react-router-dom";
 
-const App = () => {
-
-  // const location = useLocation();
-
-  // Check if the current pathname matches the pattern
-  // const isManagementRoute =  location.pathname.startsWith('/management');
-
-  const isManagementRoute = true;
-  
-  return (
-    <SettingsProvider>
-        <SettingsConsumer>
-          {({ settings }) => {
-            if(!isManagementRoute){
-              settings = {...settings,mode:'light'}
-            }
-            return <ThemeComponent settings={settings}>
-                <RouterProvider router={ApplicationRoutes}/>
-            </ThemeComponent>
-          }}
-        </SettingsConsumer>
-      </SettingsProvider>
-  )
-
-        }
+const App = () => <RouterProvider router={ApplicationRoutes}/>;
 export default App
