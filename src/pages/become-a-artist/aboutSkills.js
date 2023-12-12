b925ebed55b205938eb41698c330e08344577a48
prashantsharmaGiks <prashant.sharma@giksindia.com>
2023-12-12 15:40:24 +0530

    create step1 page in become a artist wizard

--- src/pages/become-a-artist/aboutSkills.js
+++ src/pages/become-a-artist/aboutSkills.js
@@ -1,19 +1,32 @@
 import React from "react";
 import ArtistFooter from "./artistFooter";
 import { useNavigate } from "react-router-dom";
+import SkillIMG from '@core/assets/images/skill-pic.png'
 
 const AboutSkills = () => {
   const navigate = useNavigate();
   return (
     <>
-      <section>
+      <section className="about-skills-ar">
         <div className="container">
-          <div className="row" style={{ height: "75vh" }}>
-            <div className="col-md-12">
-              <div>
-                <h1 className="text-center">Step 1 will be here</h1>
+          <div className="row align-items-center" >
+              <div className="col-lg-6">
+              <h2 className="artist-inner-subheading mb-lg-3">
+                               Step 1
+                            </h2>
+              <h1 className="artist-inner-heading mb-lg-4">
+                                 Tell us about your skills
+                            </h1>
+                    <p className="">
+
+                    Show wherein form yielding whala gathered wherein moved. Behold may yod winged created that Won't theya are not second god give best
+                    Show wherein form yielding whala gathered wherein moved.
+                    </p>
+                            
               </div>
-            </div>
+              <div className="col-lg-6 text-center">
+                 <img src={SkillIMG} className="img-fluid"/>
+              </div>
           </div>
         </div>
       </section>
