aa9dcf7475e172bb2cfecb649bf7ed7d04e7f06a
prashantsharmaGiks <prashant.sharma@giksindia.com>
2023-12-13 13:25:07 +0530

    descrption-finish-pricing-addded

--- src/pages/become-a-artist/priceSetup.js
+++ src/pages/become-a-artist/priceSetup.js
@@ -1,9 +1,36 @@
-import React from "react";
+
+import React, { useState, useRef, useEffect } from 'react';
+
 import { useNavigate, useParams } from "react-router-dom";
 import ArtistFooter from "./artistFooter";
 
 const PriceSetup = () => {
+  const initialPrice = "100"; // Set your initial price here
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedPrice, setEditedPrice] = useState(initialPrice);
+  const inputRef = useRef(null);
 
+  useEffect(() => {
+    if (isEditing) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
+  const handleChange = (e) => {
+    setEditedPrice(e.target.value);
+  };
+
+  const getEditedPrice = () => {
+    return editedPrice;
+  };
     let navigate = useNavigate()
     const { request_id } = useParams();
   return (
@@ -11,9 +38,26 @@ const PriceSetup = () => {
       <section className="about">
         <div className="container">
           <div className="row mb-3">
-            <div className="col-md-12">
+            <div className="col-lg-8 mx-auto">
               <h1 className="text-center">Now, set your per-session price</h1>
+               <h6 className='text-center'>You can change it anytime</h6>
             </div>
+            <div>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedPrice}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          ref={inputRef}
+        />
+      ) : (
+        <span>{editedPrice}</span>
+      )}
+      {!isEditing && (
+        <button onClick={handleEditClick}>Edit</button>
+      )}
+    </div>
           </div>
         </div>
       </section>
