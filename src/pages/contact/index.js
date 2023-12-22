b2795e979b6569d9e96241534f91dd60da2d883a
Shubham Semwal <shubham@Shubhams-MacBook-Pro.local>
2023-12-22 11:33:32 +0530

    create contact page

---
+++ src/pages/contact/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <section>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-12'>
+                    <div className='usr-contact-heading'>
+                        <h1 className='usr-contact text-center'>contact us</h1>
+                        <p>let’s get in touch</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default ContactPage
