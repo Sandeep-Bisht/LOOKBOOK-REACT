fe49cf2d9c50891ad3cc0c3bfe2f5b1417e51f89
Saurabh135790 <saurabh.panwar@giksindia.com>
2024-01-12 17:41:36 +0530

    design artist page

--- src/layouts/components/header/artist-filter.js
+++ src/layouts/components/header/artist-filter.js
@@ -6,14 +6,14 @@ import Slider from "react-slick";
 
 const ArtistFilter = ({ services }) => {
 
-    let navigate = useNavigate()
+  let navigate = useNavigate()
 
   const settings = {
     dots: false,
     infinite: true,
     arrows: true,
     speed: 500,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     // autoplay:true,
     // centerMode: true,
@@ -22,36 +22,36 @@ const ArtistFilter = ({ services }) => {
   };
 
 
-  const getArtistByServiceID = async (service_id) => {    
-        navigate(`/artists/${service_id}`)
+  const getArtistByServiceID = async (service_id) => {
+    navigate(`/artists/${service_id}`)
   };
 
   return (
     <section className="usr-artist-filter">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">            
+          <div className="col-lg-12">
             <Slider className="" {...settings}>
-            <div
-            onClick={()=>navigate("/artists")}
-            >
+              <div
+                onClick={() => navigate("/artists")}
+              >
                 All Artist
-            </div>
+              </div>
               {services && services.length > 0 &&
                 services.map((item, index) => {
-                  return (                    
+                  return (
                     <div
                       key={index}
-                       onClick={() => getArtistByServiceID(item._id)}
+                      onClick={() => getArtistByServiceID(item._id)}
                       className="d-flex"
                     >
                       <div className="">
-                        <img src={item.icon.thumbnailUrl} className="img-fluid service-icons" />
+                        <img src={item?.icon?.thumbnailUrl} className="img-fluid service-icons" />
                       </div>
                       <span>
-                      {item.title}
+                        {item.title}
                       </span>
-                      
+
                     </div>
                   );
                 })}
