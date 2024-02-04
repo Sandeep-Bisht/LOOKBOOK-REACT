4b2a28354a19d59902aebcbf8d16b482b9eca935
Deepak Negi <deepak.negi.giksindia.com>
2024-02-04 17:28:12 +0530

    change the route fo all services page and change z-index of header inside the services page

--- src/pages/artistSingle/index.js
+++ src/pages/artistSingle/index.js
@@ -370,7 +370,9 @@ const ArtistSingle = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="usr-artist-charges">
+                                            {
+                                                artistData.pricing && Array.isArray(artistData.pricing) &&
+                                                <div className="usr-artist-charges">
                                                 <h6 className="fw-700">Charges:</h6>
                                                 {
                                                         artistData.pricing.map((item, index) => {
@@ -385,13 +387,11 @@ const ArtistSingle = () => {
                                                             );
                                                         }
 
-                                                        return null; // You may want to handle the case where the service is not found.
+                                                        return null;
                                                     })
                                                 }
-                                                {/* <p>Wedding Function:<span className="font-style-italic">35000/-</span></p>
-                                                <p>Bridal Makeup:<span className="font-style-italic">45000/-</span></p>
-                                                <p>Bridal Relatives:<span className="font-style-italic">20000/-</span></p> */}
                                             </div>
+                                            }
                                             <div className="usr-artist-order">
                                                 <h6 className="fw-700">Minimum Order:</h6>
                                                 <p>Bridal Makeup + One Party Makeup</p>
