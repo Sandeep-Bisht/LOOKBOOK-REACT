7b94d73e3c0ac8fca211cd628140c4627ffed5cf
Shubham Semwal <shubham@Shubhams-MacBook-Pro.local>
2024-02-19 10:52:41 +0530

    create about page

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -29,20 +29,30 @@ const AboutUS = () => {
                 <div className='col-md-4'>
                    <div className='usr-about-content'>
                     <img src='images/Snapinsta 2.jpg' className='img-fluid'/>
-                    <p className='usr-about-main-para'>Meet the Team</p>
-                    <p className='usr-common-para'>Meet the faces behind Lookbook. Our team brings together a diverse blend of talents, from seasoned professionals in the beauty industry to 
-                    tech enthusiasts dedicated to creating a cutting-edge platform.</p>
+                    <p className='usr-about-main-para'>Our story</p>
+                    <p className='usr-common-para'>Founded in 2024, Lookbook was born out of a desire 
+                        to simplify the process of finding and booking talented makeup artists.
+                         Exasperated by the lack of a centralised platform, our founder 
+                         envisioned a space where artists could showcase their skills, 
+                         and clients could discover the perfect match for their beauty needs</p>
                     </div> 
                 </div>
                 <div className='col-md-4'>
                     <div className='usr-about-us-text'>
                     <div className='usr-about-para-text'>
-                        <p className='usr-about-main-para'>Our story </p>
-                        <p className='usr-common-para'>Founded in 2024, Lookbook was born out of a desire 
-                        to simplify the process of finding and booking talented makeup artists.
-                         Exasperated by the lack of a centralised platform, our founder 
-                         envisioned a space where artists could showcase their skills, 
-                         and clients could discover the perfect match for their beauty needs</p>
+                        <p className='usr-about-main-para'>Where beauty meets talent. </p>
+                        <p className='usr-common-para'>We're passionate about connecting 
+                        makeup artists with clients to createunforgettable beauty experiences.
+                        We believe that beauty is a form of art, and every individual deserves 
+                        to feelconfident and empowered. Our platform serves as the bridge between 
+                        skilledmakeup artists and clients seeking professional beauty services.
+                         Whether you're amakeup enthusiast or a seasoned artist, we're here to 
+                         make beauty accessible andenjoyable for everyone</p>
+                         <p className='usr-about-main-para'>Mission </p>
+                         <p className='usr-common-para'>At Lookbook, our mission is to 
+                         empower makeup artists and beauty enthusiasts byproviding a seamless 
+                         platform for connecting talent with individuals seekingprofessional 
+                         makeup services. We believe that everyone deserves to look and feeltheir best</p>
                     </div>
                     </div>
                 </div>
@@ -55,11 +65,39 @@ const AboutUS = () => {
         </div>
        </section>
 
-       <section className='pt-5'>
+
+       <section className='usr-meet-team-section'>
         <div className='container'>
+            <div className='row usr-team-member-meet'>
+            <p className='usr-about-main-para text-center'>Meet the Team</p>
+            <p className='usr-common-para'>Meet the faces behind Lookbook. 
+            Our team brings together a diverse blend oftalents, from seasoned 
+            professionals in the beauty industry to tech enthusiasts dedicated 
+            to creating a cutting-edge platform.</p>
+
+            <div className='col-md-3'>
+               
+                    <img src='images/aboutTeam.jpg' className='img-fluid' alt='image'/>
+                    <p className='usr-common-para text-center'>Member 1</p>
+                
+            </div>
+            <div className='col-md-3'><img src='images/meetTeam.jpg' className='img-fluid' alt='image'/>
+            <p className='usr-common-para text-center'>Member 2</p>
+            </div>
+            <div className='col-md-3'><img src='images/meetAbout.jpg' className='img-fluid' alt='image'/>
+            <p className='usr-common-para text-center'>Member 3</p>
+            </div>
+            <div className='col-md-3'><img src='images/teamMeet.jpg' className='img-fluid' alt='image'/>
+            <p className='usr-common-para text-center'>Member 4</p>
+            </div>
+                </div>
+                </div>
+       </section>
+
+       <section className='pt-5'>
+        <div className='container bg-white'>
             <div className='row'>
-            <p className='usr-about-main-para text-center'>Why Choose Us</p>
-                <div className='col-md-4 mx-auto'>
+                <div className='col-md-6'>
                 
                     <div className='usr-about-image-section'>
                        
@@ -67,13 +105,99 @@ const AboutUS = () => {
                     </div>
                     
                 </div>
-                <div className='col-md-12 mb-5'>
+                <div className='col-md-6 usr-about-value-section'>
+                
+                    <div className='usr-about-value-part'>
+                       
+                    <p className='usr-about-main-para'>Values</p>
+                    <p className='usr-common-para'>We are committed to 
+                    fostering a community built on trust, inclusivity, 
+                    and creativity.Our values drive every decision we 
+                    make, ensuring that both makeup artists andclients 
+                    have a positive and empowering experience.</p>
+                    <p className='usr-about-main-para'>Why Choose Us</p>
+                    <p className='usr-common-para'>Choosing Lookbook means
+                     gaining access to a curated selection of top-tier makeupartists, 
+                     hassle-free booking, and a platform designed with your convenience 
+                     in mind.We prioritise quality, reliability, and personalization
+                      in every beauty experience</p>
+                    </div>
+                    
+                </div>
+             
+            </div>
+        </div>
+       </section>
+
+       <section>
+        <div className='container'>
+            <div className='row usr-testimonials-path'>
+            <p className='usr-about-main-para text-center'>Testimonials</p>
+            <p className='usr-common-para'>Discover what makeup artists and 
+            clients are saying about their experiences with Lookbook. Our
+             community thrives on positive connections, and we're proud to share their stories</p>  
+             <div className='col-md-4'><div className='testimonial-text'>
+                <img src='images/Frame 139.png'/>
+                <p className='usr-common-para'>Lorem ipsum dolor sit amet consectetur. Id magna amet egestas sit
+                     euismod. Facilisis tristique elementum fermentum non sed 
+                     interdum auctor malesuada quis. Ut venenatis tellus tellus 
+                     pharetra et at. Lobortis.</p>
+                     <img src='images/Frame 140.png'/>
+                     </div></div>
+             <div className='col-md-4'>
+             <div className='testimonial-text'>
+                <p className='usr-common-para'>Lorem ipsum dolor sit amet consectetur. Id magna amet egestas sit
+                     euismod. Facilisis tristique elementum fermentum non sed 
+                     interdum auctor malesuada quis. Ut venenatis tellus tellus 
+                     pharetra et at. Lobortis.</p></div>
+             </div>
+             <div className='col-md-4'>
+             <div className='testimonial-text'>
+                <p className='usr-common-para'>Lorem ipsum dolor sit amet consectetur. Id magna amet egestas sit
+                     euismod. Facilisis tristique elementum fermentum non sed 
+                     interdum auctor malesuada quis. Ut venenatis tellus tellus 
+                     pharetra et at. Lobortis.</p></div>
+             </div>
+            </div>
+        </div>
+       </section>
+
+       <section className='py-5'>
+        <div className='container bg-white'>
+            <div className='row'>
+                <div className='col-md-8'>
+                    <div className='usr-beauty-journey-text'>
+                <p className='usr-about-main-para'>Join Us on the Beauty Journey</p>
+                    <p className='usr-common-para'>Whether you're a makeup artist ready
+                     to showcase your skills or a client looking forthe perfect beauty 
+                     experience, we invite you to join us on this exciting beauty journey.
+                     At Lookbook, beauty knows no bounds, and every face tells a unique story.
+                     Ready to elevate your beauty experience? Join Lookbook today! Makeup 
+                     artists,showcase your talent to a broader audience. Clients, discover
+                      the perfect artist for your next beauty transformation. Let's create beauty together!</p>
+                </div></div>
+                <div className='col-md-4'>
+                    <div className='usr-beauty-journey-text-right'>
+                <p className='usr-about-main-para'>Contact Info</p>
+                    <p className='usr-common-para'>Have questions or want to learn more? Reach out to
+                     us at info@mylookbook.in or follow us on Instagram. We're here to make
+                      your beauty journey exceptional</p>
+                </div></div>
+                
+                    
+            </div>
+        </div>
+       </section>
+
+       <section>
+        <div className='container'>
+            <div className='row'>
+            <div className='col-md-12 mb-5'>
                     <div className='text-center mt-3'>
                     <div className='usr-about-text-para'>
-                        <p className='usr-common-para ps-4 pe-4'>Choosing Lookbook means gaining access to a curated selection of 
-                        top-tier makeup artists, hassle-free booking, and a platform designed with your 
-                        convenience in mind. We prioritise quality, reliability, and personalization in every 
-                        beauty experience</p></div></div>
+                        <p className='usr-common-para-last ps-4 pe-4'>Ready to discover the 
+                        perfect makeup artist or showcase your talent? 
+                        Let's embark on this beauty adventure together!</p></div></div>
             </div>
             </div>
         </div>
