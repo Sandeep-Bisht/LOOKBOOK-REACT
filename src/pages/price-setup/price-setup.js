5133e81da6aa1c59f56071bc6fd303e10f48cf23
Sandeep Bisht <>
2024-02-23 16:33:49 +0530

    update service

--- src/pages/price-setup/price-setup.js
+++ src/pages/price-setup/price-setup.js
@@ -9,12 +9,19 @@ import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import "@css/user/cart.css";
 import { toast } from "react-toastify";
+import Button from '@mui/material/Button'
 
 const SetupPrice = () => {
   const artist = useLoaderData();
   const [artistData, setArtistData] = useState(artist);
+  const [editServiceData, setEditServiceData] = useState()
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { register: register2, handleSubmit: handleSubmit2, formState: { errors: errors2 }  } = useForm();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [editMode, setEditMode] = useState(false)
+  const [iconUrl, setIconUrl] = useState(editServiceData?.icon?.thumbnailUrl)
+  const [selectFileIcon, setSelectFileIcon] = useState()
+  
 
   const  generateUniqueId = async () => {
     const currentDate = new Date();
@@ -73,6 +80,34 @@ const SetupPrice = () => {
     }
  
   }
+
+  const handleEditService = async(service) => {
+    reset()
+    setEditMode(true);
+    setIconUrl(service?.icon?.thumbnailUrl)
+    setEditServiceData(service)
+  }
+
+  const updateForm = async(data) =>{
+    console.log("inside update ", data)
+  }
+
+  const handleFileChangeIcon = (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    reader.onload = function (event) {
+      const imageUrl = event.target.result;
+      setIconUrl(imageUrl);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+      setSelectFileIcon(file);
+    }
+  };
+
+  
   return (
     <section className="artist-set-price-wrapper user-cart-section">
       <div className="container">
@@ -116,7 +151,7 @@ const SetupPrice = () => {
                           </Td>
                           <Td className="text-capitalize">
                             <div className="action-btn-wrapper">
-                              <button className="action-btn">
+                              <button className="action-btn" onClick={()=>handleEditService(item)}>
                                 <CiEdit />
                               </button>
                               <button className="action-btn" onClick={()=> deleteArtistService(item?._id)}>
@@ -132,6 +167,7 @@ const SetupPrice = () => {
               </div>
             )}
           <div className="col-lg-3">
+            {!editMode ?
             <form
               onSubmit={handleSubmit(submitForm)}
               className="artist-set-price-form"
@@ -164,7 +200,7 @@ const SetupPrice = () => {
                   type="text"
                   name="price"
                   id="price"
-                  defaultValue={artist?.pricing?.price}
+                  // defaultValue={artist?.pricing?.price}
                   className="form-control"
                   {...register("price", { 
                     required: "Price is required.",
@@ -181,7 +217,7 @@ const SetupPrice = () => {
                 <input
                   type="number"
                   name="sessionTime"
-                  defaultValue={artist?.pricing?.sessionTime}
+                  // defaultValue={artist?.pricing?.sessionTime}
                   id="sessionTime"
                   className="form-control"
                   {...register("sessionTime", { 
@@ -197,7 +233,7 @@ const SetupPrice = () => {
                    })}
                 />
                  {errors.sessionTime && <p className="text-danger">{errors.sessionTime.message}</p>}
-              </div>
+              </div>             
               <button
                 type="submit"
                 className="btn-success usr-common-action-btn mt-5"
@@ -206,6 +242,126 @@ const SetupPrice = () => {
                 {isSubmitting ? 'Submitting...' : 'Submit'}
             </button>
             </form>
+              :
+                   
+            <form
+              onSubmit={handleSubmit2(updateForm)}
+              className="artist-set-price-form"
+            >
+              <div className="form-group mb-3">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  defaultValue={editServiceData && editServiceData?.title}
+                  className="form-control"
+                  {...register2("title", { required: "Title is required." })}
+                />
+                 {errors2.title && <p className="text-danger">{errors2.title.message}</p>}
+              </div>
+              {/* <div className="form-group ">
+                <label htmlFor="icon">Icon</label>
+                <input
+                  type="file"
+                  name="icon"
+                  id="icon"
+                  defaultValue={editServiceData?.icon?.name}
+                  className="form-control"
+                  {...register2("icon", { required: "Icon is required." })}
+                />
+                 {errors2.icon && <p className="text-danger">{errors2.icon.message}</p>}
+              </div>
+               */}
+               <div>
+                <img 
+                src={editServiceData?.icon?.thumbnailUrl}
+                alt='icon'
+                style={{ height: '100px', width: '100px' }}
+                />
+                 <Button
+                    component="label"
+                    variant="contained"
+                    className="mt-2"
+                    htmlFor="icon-image"
+                  >
+                    Upload
+                    <input
+                      hidden
+                      type="file"
+                      id="icon-image"
+                      onChange={handleFileChangeIcon}
+                      accept="image/*"
+                    />
+                  </Button>
+
+                  <Button variant="contained" className="ms-2 mt-2" color="secondary" onClick={() =>
+                     { 
+                      setIconUrl(editServiceData?.icon.thumbnailUrl)
+                      setSelectFileIcon("")}}
+                      >
+                    Reset
+                  </Button>
+               </div>
+              <div className="form-group mb-3">
+                <label htmlFor="title">Price</label>
+                <input
+                  type="text"
+                  name="price"
+                  defaultValue={editServiceData && editServiceData?.pricing?.price}
+                  id="price"                 
+                  className="form-control"
+                  {...register2("price", { 
+                    required: "Price is required.",
+                    min: {
+                      value: artist?.pricing?.price,
+                      message: `Minimum Price should be ${artist?.pricing?.price}`
+                  }
+                   })}
+                />
+                 {errors2.price && <p className="text-danger">{errors2.price.message}</p>}
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="title">Session</label>
+                <input
+                  type="number"
+                  name="sessionTime"
+                  defaultValue={editServiceData?.pricing?.sessionTime}
+                  id="sessionTime"
+                  className="form-control"
+                  {...register2("sessionTime", { 
+                    required: "Session is required.",
+                    min: {
+                      value: 1,
+                      message: "Session must be at least 1hrs."
+                  },
+                  max: {
+                    value: 5,
+                    message: "Session cannot exceed 5hrs."
+                }
+                   })}
+                />
+                 {errors2.sessionTime && <p className="text-danger">{errors2.sessionTime.message}</p>}
+              </div>             
+              <button
+                type="submit"
+                className="btn-success usr-common-action-btn mt-5"
+                disabled={isSubmitting}
+            >
+                {isSubmitting ? 'Updating...' : 'Update'}
+            </button>
+            <button
+            onClick={()=>{
+              setEditMode(false)
+              setEditServiceData()
+            }}
+                type="button"
+                className="btn-success usr-common-action-btn"
+            >
+                Cancel
+            </button>
+            </form>
+                  }
           </div>
         </div>
       </div>
