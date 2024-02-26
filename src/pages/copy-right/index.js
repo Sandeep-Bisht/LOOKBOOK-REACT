65bf0c5cb44e6be499a04e6c8f52bb664d1bab91
Shubham Semwal <shubham@Shubhams-MacBook-Pro.local>
2024-02-26 18:15:33 +0530

    create copyrights page

---
+++ src/pages/copy-right/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '@css/user/copyright.css'
+const CopyRights = () => {
+  return (
+    <div>
+
+        <section className='p-5'>
+            <div className='container'>
+                <div className='row'>
+                 <div className='col-md-10'>
+                 <h1 className='usr-copy-right-head'>COPYRIGHT NOTICE</h1>
+        <p className='usr-copy-right-para'>Unless otherwise indicated, all materials on these pages are copyrighted by Lookbook. All rights reserved.</p>
+        <p className='usr-copy-right-para'>No part of these pages, either text or image, may be used for any purpose other than personal use. Therefore, reproduction, modification, storage in a retrieval system, or retransmission, in any form or by any means, electronic, mechanical, or otherwise, for reasons other than personal use, is strictly prohibited without prior written permission.</p>
+        <p className='usr-copy-right-para'>For permission to use any of the material on this website for purposes other than personal use, please contact Lookbook.</p>
+        <p className='usr-copy-right-para'>Copyright © [2024] Lookbook. All Rights Reserved.</p>
+
+                 </div>
+                </div>
+            </div>
+        </section>
+      
+    </div>
+  )
+}
+
+export default CopyRights
